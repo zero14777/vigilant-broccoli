4c070ef2081ee4743cde35ba75546bded11fe67b
Pbgreerb <pbgreerb@gmail.com>
2016-09-11 14:42:05 -0700

    Setting up project

---
+++ Game/Assets/Scripts/Player.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	private Rigidbody2D m_player_body;
+	[SerializeField]
+	private float m_speed = 10;
+
+	// Use this for initialization
+	private void Start () {
+		m_player_body = gameObject.GetComponent<Rigidbody2D> ();
+	}
+	
+	// Update is called once per frame
+	private void Update () {
+		GetInput ();
+	}
+
+	private void GetInput () {
+		float horizontal_move = Input.GetAxis ("Horizontal") * m_speed;
+		m_player_body.AddForce (Vector3.right * horizontal_move);
+	}
+}
