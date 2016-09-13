0e1291f3982aecd9670c76c0ba5bcff0569b5ace
Pbgreerb <pbgreerb@gmail.com>
2016-09-13 00:33:06 -0700

    added simple controls

--- Game/Assets/Scripts/Player.cs
+++ Game/Assets/Scripts/Player.cs
@@ -1,24 +1,72 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Player : MonoBehaviour {
+public class Player : MonoBehaviour
+{
+	public Transform m_left_foot;
+	public Transform m_right_foot;
+	public float m_hops = 8.0f;
+	public float m_speed = 5.0f;
+	public float m_weight = 1.0f;
 
-	private Rigidbody2D m_player_body;
-	[SerializeField]
-	private float m_speed = 10;
+	private Rigidbody2D m_player;
+	private bool grounded = false;
+	private Vector2 m_move;
 
 	// Use this for initialization
-	private void Start () {
-		m_player_body = gameObject.GetComponent<Rigidbody2D> ();
-	}
-	
-	// Update is called once per frame
-	private void Update () {
-		GetInput ();
+	void Start ()
+	{
+		m_player = GetComponent<Rigidbody2D> (); 
+		m_move = new Vector2(0.0f, 0.0f);
 	}
 
-	private void GetInput () {
-		float horizontal_move = Input.GetAxis ("Horizontal") * m_speed;
-		m_player_body.AddForce (Vector3.right * horizontal_move);
+	// Update is called once per frame
+	void Update ()
+	{
+	}
+
+	void FixedUpdate ()
+	{
+		if (!grounded) {
+			grounded = Physics2D.Linecast (m_left_foot.position, m_right_foot.position, LayerMask.GetMask ("Default"));
+		}
+		if (grounded) {
+			movement ();
+			if (Input.GetKeyDown ("w")) {
+				grounded = false;
+				m_player.AddForce ((Vector2.up * m_hops), ForceMode2D.Impulse);
+			}
+		} else {
+			airdrift ();
+		}
+	}
+
+	private void movement ()
+	{
+		m_move.x = 0.0f;
+		if (Input.GetKey ("d"))
+		{
+			m_move.x = m_speed;
+		}
+		if (Input.GetKey ("a"))
+		{
+			m_move.x -= m_speed;
+		}
+		m_player.velocity = m_move;
+	}
+
+	private void airdrift ()
+	{
+		Vector2 velocity_vect = m_player.velocity;
+		velocity_vect.x = 0.0f;
+		if (Input.GetKey ("d"))
+		{
+			velocity_vect.x = (5.0f / m_weight);
+		}
+		if (Input.GetKey ("a"))
+		{
+			velocity_vect.x -= (5.0f / m_weight);
+		}
+		m_player.velocity = velocity_vect;
 	}
 }
