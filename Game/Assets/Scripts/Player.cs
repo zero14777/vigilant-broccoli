66436f5b0611a81cf6b4952538820c824ede1729
Pbgreerb <pbgreerb@gmail.com>
2016-10-14 16:01:50 -0700

    .

--- Game/Assets/Scripts/Player.cs
+++ Game/Assets/Scripts/Player.cs
@@ -1,23 +1,19 @@
 ﻿using UnityEngine;
+using System;
 using System.Collections;
 
-public class Player : MonoBehaviour
+public class Player : Unit
 {
 	public Transform m_left_foot;
 	public Transform m_right_foot;
 	public float m_hops = 8.0f;
 	public float m_speed = 5.0f;
-	public float m_weight = 1.0f;
 
 	private Rigidbody2D m_player;
-	private bool grounded = false;
-	private Vector2 m_move;
-
 	// Use this for initialization
 	void Start ()
 	{
-		m_player = GetComponent<Rigidbody2D> (); 
-		m_move = new Vector2(0.0f, 0.0f);
+		m_player = GetComponent<Rigidbody2D> ();
 	}
 
 	// Update is called once per frame
@@ -27,45 +23,27 @@ public class Player : MonoBehaviour
 
 	void FixedUpdate ()
 	{
-		if (!grounded) {
-			grounded = Physics2D.Linecast (m_left_foot.position, m_right_foot.position, LayerMask.GetMask ("Default"));
-		}
-		if (grounded) {
-			movement ();
-			if (Input.GetKeyDown ("w")) {
-				grounded = false;
-				m_player.AddForce ((Vector2.up * m_hops), ForceMode2D.Impulse);
-			}
-		} else {
-			airdrift ();
-		}
+		movement ();
+	}
+
+	private bool grounded () {
+		return Physics2D.Linecast (m_left_foot.position, m_right_foot.position, LayerMask.GetMask ("Default"));
 	}
 
 	private void movement ()
 	{
-		m_move.x = 0.0f;
-		if (Input.GetKey ("d"))
-		{
-			m_move.x = m_speed;
+		if (Input.GetKeyDown ("w") && grounded ()) {
+			m_player.AddForce ((Vector2.up * m_hops), ForceMode2D.Impulse);
 		}
-		if (Input.GetKey ("a"))
-		{
-			m_move.x -= m_speed;
-		}
-		m_player.velocity = m_move;
-	}
-
-	private void airdrift ()
-	{
 		Vector2 velocity_vect = m_player.velocity;
 		velocity_vect.x = 0.0f;
 		if (Input.GetKey ("d"))
 		{
-			velocity_vect.x = (5.0f / m_weight);
+			velocity_vect.x = (5.0f);
 		}
 		if (Input.GetKey ("a"))
 		{
-			velocity_vect.x -= (5.0f / m_weight);
+			velocity_vect.x -= (5.0f);
 		}
 		m_player.velocity = velocity_vect;
 	}
---
+++ Game/Assets/Scripts/Viking_Boss.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+
+public class Viking_Boss : MonoBehaviour {
+
+	enum Attack {none, running, wait, charge, spin};
+
+	private Attack next_attack = Attack.none;
+	private System.Random random = new System.Random ();
+	private Attack[] attacks;
+
+	// Use this for initialization
+	void Start () {
+		attacks = (Attack[])Enum.GetValues (typeof(Attack));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (next_attack == Attack.none) {
+			next_attack = (Attack)attacks.GetValue (random.Next (attacks.Length - 1) + 1);
+			DoAttack ();
+		}
+	}
+
+	void DoAttack () {
+		//Do Stuff
+	}
+}
